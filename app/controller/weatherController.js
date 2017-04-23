58e65bd53be89ce5829234c12e9adb3fe5044934
n-i-k-h-i-l <nikhilchawla.12@gmail.com>
2017-04-23 16:39:40 +0530

    filter added

--- app/controller/weatherController.js
+++ app/controller/weatherController.js
@@ -1,6 +1,6 @@
-app.controller('weatherController', ['$http', '$scope', 'weatherService', 'weatherFactory','GetWeatherData',
+app.controller('weatherController', ['$http', '$scope', 'weatherService', 'weatherFactory','GetWeatherData','$filter',
 
-    function($http, $scope, weatherService, weatherFactory,GetWeatherData){
+    function($http, $scope, weatherService, weatherFactory,GetWeatherData,$filter){
         var apiId = '084c66197563211ec73423a8f2b503fc';
         var data = {};
         weatherService.getCurrentPosition().then(function(response){
@@ -42,19 +42,24 @@ app.controller('weatherController', ['$http', '$scope', 'weatherService', 'weath
             var url = 'http://api.openweathermap.org/data/2.5/forecast?lat='+data.lat+'&lon='+data.lon+'&mode=json&appid='+apiId+'&units=metric';
             
             weatherService.getJsonFromAPI(url).then(function(response){
+                console.log(response.list);
                 $scope.forecastList = response.list;
 				count = 0;
                 if($scope.forecastList.length > 0){
 					
                     angular.forEach($scope.forecastList,function(value,index){
-											
+						$scope.forecastList[index].maxTemp = value.main.temp_max;
+                        $scope.forecastList[index].minTemp = value.main.temp_min;
                         $scope.forecastList[index].degree = value.main.temp;
                         $scope.forecastList[index].weatherIcon = "http://openweathermap.org/img/w/" + value.weather[0].icon + ".png";
-                        $scope.forecastList[index].time = value.dt_txt;
+                        $scope.forecastList[index].time = $filter('date')(new Date(value.dt_txt),'yyyy-MM-dd');
 						$scope.forecastList[index].cond = value.weather[0].main + ' ( ' + value.weather[0].description + ' )';
 						//alert($scope.forecastList[index].weatherIcon);
                        count++;
-                    })
+                    });
+                    $scope.filteredList= $scope.filterWeatherData($scope.forecastList);
+                    console.log("Filetered List !!!");
+                    console.log($scope.filteredList);
 					//alert(count);
                 }
 				
@@ -62,5 +67,46 @@ app.controller('weatherController', ['$http', '$scope', 'weatherService', 'weath
             }, function(response){
                 
             });
-        }
+        };
+        
+         $scope.filterWeatherData = function (temperatureData) {
+           // var temperatureData = weatherData.degree;
+            var localData = {
+                date: "",
+                maxTemp: 0.0,
+                minTemp: 1000,
+                description: "",
+                weatherIcon: "",
+                degree:""
+            };
+            var returnTemperatureData = [];
+            var localDate = $filter('date')(new Date(), 'yyyy-MM-dd');
+            console.log(localDate);
+            localData.time = "Today";
+             localData.degree= temperatureData[0].degree;
+            localData.cond = temperatureData[0].cond;
+            localData.weatherIcon = temperatureData[0].weatherIcon;
+            for (var i = 0; i < temperatureData.length; i++) {
+                var eachData = temperatureData[i];
+                if (localDate == eachData.time) {
+                    if (localData.maxTemp < eachData.maxTemp) {
+                        localData.maxTemp = eachData.maxTemp;
+                    }
+                    if (localData.minTemp > eachData.minTemp) {
+                        localData.minTemp = eachData.minTemp;
+                    }
+                } else {
+                    var localDataCopy=angular.copy(localData);
+                    returnTemperatureData.push(localDataCopy);
+                    localData.time = eachData.time;
+                    localData.maxTemp = eachData.maxTemp;
+                    localData.minTemp = eachData.minTemp;
+                    localData.cond = eachData.cond;
+                    localData.weatherIcon = eachData.weatherIcon;
+                    localData.degree = eachData.degree;
+                }
+                localDate = eachData.time;
+            }
+            return returnTemperatureData;
+        };
 }])
--- app/services/weatherService.js
+++ app/services/weatherService.js
@@ -29,5 +29,42 @@ app.service('weatherService', ['$http', '$q','$filter', '$window',function($http
         return deferred.promise;
     }
 
+    this.filterWeatherData = function (weatherData) {
+            var temperatureData = weatherData.temperature;
+            var localData = {
+                date: "",
+                maxTemp: 0.0,
+                minTemp: 1000,
+                description: ""
+            };
+            var returnTemperatureData = [];
+            var localDate = $filter('date')(new Date(), 'yyyy-MM-dd');
+            console.log(localDate);
+            localData.date = "Today";
+            localData.description = temperatureData[0].desc;
+            for (var i = 0; i < temperatureData.length; i++) {
+                var eachData = temperatureData[i];
+                if (localDate == eachData.date) {
+                    if (localData.maxTemp < eachData.maxTemp) {
+                        localData.maxTemp = eachData.maxTemp;
+                    }
+                    if (localData.minTemp > eachData.minTemp) {
+                        localData.minTemp = eachData.minTemp;
+                    }
+                } else {
+                    var localDataCopy=angular.copy(localData);
+                    returnTemperatureData.push(localDataCopy);
+                    localData.date  = eachData.date;
+                    localData.maxTemp = eachData.maxTemp;
+                    localData.minTemp = eachData.minTemp;
+                    localData.description = eachData.desc;
+                }
+                localDate = eachData.date;
+            }
+            return returnTemperatureData;
+        };
+
+    
+    
 }]);
 
